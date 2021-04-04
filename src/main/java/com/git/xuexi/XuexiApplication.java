dcd42bcb640888a27c9abf036f4f68cd5351e40f
jjs1017 <jjs19970611@163.com>
2021-04-04 17:19:47 +0800

    squash! 修改

---
+++ src/main/java/com/git/xuexi/XuexiApplication.java
@@ -0,0 +1,13 @@
+package com.git.xuexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuexiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuexiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/git/xuexi/XuexiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.xuexi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuexiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
